8befcb701bcecb58cbe3169cbc2c91a6a4227be4
Smcclean4 <lsaudim5@outlook.com>
2022-10-07 12:21:03 -0700

    docker setup
    setup adminer for mysql and also setup apache server.

--- index.php
+++
@@ -1,4 +0,0 @@
-<?php
-
-echo ("Hello PH-eople");
-echo ("Okay .. nvm");
---
+++ src/index.php
@@ -0,0 +1,7 @@
+<?php
+
+echo ("Hello PH-eople");
+echo (" Okay..nvm");
+echo (" Get that bread too");
+echo (" dont forget that mungyung");
+echo ("okay?");
