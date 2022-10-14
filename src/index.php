88fa5cf259b95ba8009f88153522418167eea3ef
Smcclean4 <lsaudim5@outlook.com>
2022-10-13 18:15:28 -0700

    PHP practice
    practicing some php and will get into building the application.

--- src/index.php
+++ src/index.php
@@ -1,7 +1,19 @@
 <?php
 
-echo ("Hello PH-eople");
-echo (" Okay..nvm");
-echo (" Get that bread too");
-echo (" dont forget that mungyung");
-echo ("okay?");
+echo "get that money man ";
+echo "just a little bit...";
+
+?>
+
+<?php
+
+$name = 'Sid';
+$age = 12;
+$has_kids = true;
+$cash_on_hand = 345.75;
+// var_dump() will show the value of boolean and arithmetic solutions
+
+echo "${name} is ${age} and he has kids so in PHP it shows up as ${has_kids} ...speaking of PHP he has ${cash_on_hand} in his wallet right now!! ";
+// define use for constants that will never change.
+define("HOST", "money");
+echo HOST;
