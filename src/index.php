23ddfe6b8838aa56ba275a4ecfe5d085d9f41de3
Smcclean4 <lsaudim5@outlook.com>
2022-12-27 22:06:14 -0800

    mroe practice

--- src/index.php
+++ src/index.php
@@ -1,3 +1,4 @@
 <div>
-  <h1><?php echo "PHP Feedback App" ?></h1> 
+  <h1><?php echo "PHP Feedback App" ?></h1>
+  <p><?php echo "This project will show feedback that users leave on your site"?></p>
 </div>
