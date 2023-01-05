decb21a994000b5df4ac25fd96630cadf775f389
Smcclean4 <lsaudim5@outlook.com>
2023-01-04 23:14:24 -0800

    feedback
    adding feedback data and connecting to database.

--- src/config/database.php
+++ src/config/database.php
@@ -1,7 +1,7 @@
 <?php
-  define('DB_HOST', 'localhost');
+  define('DB_HOST', 'db');
   define('DB_USER', 'sid');
-  define('DB_PASS', '@genTzer0');
+  define('DB_PASS', '123456');
   define('DB_NAME', 'php_dev');
 
   // create connection
--- src/feedback.php
+++ src/feedback.php
@@ -1,15 +1,56 @@
 <?php include './inc/header.php' ?>
+
+<?php
+  $feedback = [
+    [
+      'id' => '1',
+      'name' => 'John Canty',
+      'email' => 'johncanty87@icloud.com',
+      'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia,
+      molestiae quas vel sint commodi repudiandae consequuntur voluptatum laborum
+      numquam blanditiis harum quisquam eius sed odit fugiat iusto fuga praesentium
+      optio, eaque rerum! Provident similique accusantium nemo autem. Veritatis
+      obcaecati tenetur iure eius earum ut molestias architecto voluptate aliquam
+      nihil, eveniet aliquid culpa officia aut! Impedit sit sunt quaerat, odit,
+      tenetur error, harum nesciunt ipsum debitis quas aliquid.'
+    ],[
+      'id' => '2',
+      'name' => 'Marissa Vargas',
+      'email' => 'mvargas234@outlook.com',
+      'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia,
+      molestiae quas vel sint commodi repudiandae consequuntur voluptatum laborum
+      numquam blanditiis harum quisquam eius sed odit fugiat iusto fuga praesentium
+      optio, eaque rerum! Provident similique accusantium nemo autem. Veritatis
+      obcaecati tenetur iure eius earum ut molestias architecto voluptate aliquam
+      nihil, eveniet aliquid culpa officia aut! Impedit sit sunt quaerat, odit,
+      tenetur error, harum nesciunt ipsum debitis quas aliquid.'
+    ],[
+      'id' => '3',
+      'name' => 'Tim Curton',
+      'email' => 'timmyc20@hotmail.com',
+      'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia,
+      molestiae quas vel sint commodi repudiandae consequuntur voluptatum laborum
+      numquam blanditiis harum quisquam eius sed odit fugiat iusto fuga praesentium
+      optio, eaque rerum! Provident similique accusantium nemo autem. Veritatis
+      obcaecati tenetur iure eius earum ut molestias architecto voluptate aliquam
+      nihil, eveniet aliquid culpa officia aut! Impedit sit sunt quaerat, odit,
+      tenetur error, harum nesciunt ipsum debitis quas aliquid.'
+    ]
+  ]
+?>
+
 <h2>Feedback</h2>
 
-<div class="card my-3">
-  <div class="card-body">
-  Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia, molestiae quas vel sint commodi repudiandae consequuntur voluptatum laborum numquam blanditiis harum quisquam eius sed odit fugiat iusto fuga praesentium optio, eaque rerum! Provident similique accusantium nemo autem. Veritatis obcaecati tenetur iure eius earum ut molestias architecto voluptate aliquamnihil, eveniet aliquid culpa officia aut! Impedit sit sunt quaerat, odit,tenetur error, harum nesciunt ipsum debitis quas aliquid. Reprehenderit,quia. Quo neque error repudiandae fuga?
-  </div>
-</div>
+<?php if (empty($feedback)): ?>
+  <p class="lead mt-3">No feedback left behind.</p>
+<?php endif; ?>
 
+<?php foreach($feedback as $item): ?>
 <div class="card my-3">
   <div class="card-body">
-  Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia, molestiae quas vel sint commodi repudiandae consequuntur voluptatum laborum numquam blanditiis harum quisquam eius sed odit fugiat iusto fuga praesentium optio, eaque rerum! Provident similique accusantium nemo autem. Veritatis obcaecati tenetur iure eius earum ut molestias architecto voluptate aliquamnihil, eveniet aliquid culpa officia aut! Impedit sit sunt quaerat, odit,tenetur error, harum nesciunt ipsum debitis quas aliquid. Reprehenderit,quia. Quo neque error repudiandae fuga?
+    <?php echo $item['body'] ?>
   </div>
 </div>
-<?php include './inc/footer.php' ?>
+<?php endforeach; ?>
+
+<?php include './inc/footer.php'; ?>
